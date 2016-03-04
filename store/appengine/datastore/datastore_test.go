763aa9aa2c336a5ce1dfbf001c850b70789a64bc
coolwust <coolwust@gmail.com>
2016-03-03 22:06:36 -0500

    fix

--- store/appengine/datastore/datastore_test.go
+++ store/appengine/datastore/datastore_test.go
@@ -11,30 +11,30 @@ import (
 	"golang.org/x/net/context"
 )
 
-//var dataTest = Data{
-//	"foo": int64(1),
-//	"bar": float64(1.5),
-//	"baz": "qux",
-//}
-//
-//func TestData(t *testing.T) {
-//	ctx, done, err := aetest.NewContext()
-//	if err != nil {
-//		t.Fatal(err)
-//	}
-//	defer done()
-//	k := datastore.NewIncompleteKey(ctx, "data", nil)
-//	if k, err = datastore.Put(ctx, k, dataTest); err != nil {
-//		t.Fatal(err)
-//	}
-//	d := make(Data)
-//	if err := datastore.Get(ctx, k, d); err != nil {
-//		t.Fatal(err)
-//	}
-//	if !reflect.DeepEqual(dataTest, d) {
-//		t.Fatalf("got %v, want %v", d, dataTest)
-//	}
-//}
+var dataTest = Data{
+	"foo": int64(1),
+	"bar": float64(1.5),
+	"baz": "qux",
+}
+
+func TestData(t *testing.T) {
+	ctx, done, err := aetest.NewContext()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer done()
+	k := datastore.NewIncompleteKey(ctx, "data", nil)
+	if k, err = datastore.Put(ctx, k, dataTest); err != nil {
+		t.Fatal(err)
+	}
+	d := make(Data)
+	if err := datastore.Get(ctx, k, d); err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(dataTest, d) {
+		t.Fatalf("got %v, want %v", d, dataTest)
+	}
+}
 
 func TestDatastore(t *testing.T) {
 	ctx, done, err := aetest.NewContext()
